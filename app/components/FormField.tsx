592eb5a4941d9a3853fb09239f99245442912135
giftea <eleojogiftuhiene@gmail.com>
2024-01-16 15:06:57 +0100

    starter init

--- app/components/FormField.tsx
+++ app/components/FormField.tsx
@@ -1,21 +1,5 @@
+function FormField() {
+  return <></>;
+}
 
-import { FormFieldProps } from "@/types";
-
-const FormField: React.FC<FormFieldProps> = ({
-  type,
-  placeholder,
-  name,
-  register,
-  error,
-  valueAsNumber,
-}) => (
-  <>
-    <input
-      type={type}
-      placeholder={placeholder}
-      {...register(name, { valueAsNumber })}
-    />
-    {error && <span className="error-message">{error.message}</span>}
-  </>
-);
 export default FormField;
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,8 @@
 "use client";
-import Form from "./components/Form";
 
 function Home() {
- 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <Form />
     </main>
   );
 }
